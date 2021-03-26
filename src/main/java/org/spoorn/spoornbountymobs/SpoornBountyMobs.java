24e1cd7975012574d6bb784f1a1b7822c82ebbe4
spoorn <michael.trinh1996@gmail.com>
2021-03-26 00:30:59 -0700

    Initial code

---
+++ src/main/java/org/spoorn/spoornbountymobs/SpoornBountyMobs.java
@@ -0,0 +1,10 @@
+package org.spoorn.spoornbountymobs;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SpoornBountyMobs implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/org/spoorn/spoornbountymobs/client/SpoornBountyMobsClient.java
@@ -0,0 +1,13 @@
+package org.spoorn.spoornbountymobs.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class SpoornBountyMobsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
