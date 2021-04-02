affafe66887c79bb6684b8ecc086b05c556c18ea
spoorn <michael.trinh1996@gmail.com>
2021-04-02 00:57:27 -0700

    Fix mob size scaling

--- src/main/java/org/spoorn/spoornbountymobs/config/DoomTierConfig.java
+++ src/main/java/org/spoorn/spoornbountymobs/config/DoomTierConfig.java
@@ -7,8 +7,8 @@ public class DoomTierConfig {
     @Comment("Spawn weight of this tier (higher number is higher chance of spawn) [default = 1]")
     public int spawnChance = 1;
 
-    @Comment("This tier's Mob size scale [default = 2.7]")
-    public double mobSizeScale = 2.7;
+    @Comment("This tier's Mob size scale [default = 3.0]")
+    public double mobSizeScale = 3.0;
 
     @Comment("Base bonus health for this tier [default = 70]")
     public int baseBonusHealth = 70;
