af834c22833c1fdf2284ef657b8d332dfcf3831d
spoorn <michael.trinh1996@gmail.com>
2022-01-14 02:05:38 -0800

    Bump to 2.1
    Add broadcast messages when tracking a bounty mob

--- src/main/java/org/spoorn/spoornbountymobs/tiers/SpoornBountyTierTypes.java
+++ src/main/java/org/spoorn/spoornbountymobs/tiers/SpoornBountyTierTypes.java
@@ -1,13 +1,24 @@
 package org.spoorn.spoornbountymobs.tiers;
 
+import lombok.Getter;
+import net.minecraft.util.Formatting;
+
 /**
  * Tier enums.
  */
 public enum SpoornBountyTierTypes {
-    COMMON_TIER,
-    UNCOMMON_TIER,
-    RARE_TIER,
-    EPIC_TIER,
-    LEGENDARY_TIER,
-    DOOM_TIER
+    COMMON_TIER("COMMON", Formatting.GRAY),
+    UNCOMMON_TIER("UNCOMMON", Formatting.GREEN),
+    RARE_TIER("RARE", Formatting.BLUE),
+    EPIC_TIER("EPIC", Formatting.DARK_PURPLE),
+    LEGENDARY_TIER("LEGENDARY", Formatting.GOLD),
+    DOOM_TIER("DOOM", Formatting.DARK_RED);
+
+    @Getter private final String name;
+    @Getter private final Formatting[] formattings;
+
+    SpoornBountyTierTypes(String name, Formatting... formatting) {
+        this.name = name;
+        formattings = formatting;
+    }
 }
