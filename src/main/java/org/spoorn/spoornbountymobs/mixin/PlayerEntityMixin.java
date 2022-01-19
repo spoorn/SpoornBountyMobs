1b5a99cc619d06d17dbae518dd5b2f1984da9746
spoorn <spookump@gmail.com>
2022-01-19 02:34:21 -0800

    Bump to 2.3
    Add toggleable broadcast message when player's bounty level increases

--- src/main/java/org/spoorn/spoornbountymobs/mixin/PlayerEntityMixin.java
+++ src/main/java/org/spoorn/spoornbountymobs/mixin/PlayerEntityMixin.java
@@ -2,18 +2,27 @@ package org.spoorn.spoornbountymobs.mixin;
 
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.network.MessageType;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.Util;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spoorn.spoornbountymobs.config.ModConfig;
+import org.spoorn.spoornbountymobs.entity.SpoornBountyEntityRegistry;
 import org.spoorn.spoornbountymobs.entity.component.EntityDataComponent;
 import org.spoorn.spoornbountymobs.entity.component.PlayerDataComponent;
-import org.spoorn.spoornbountymobs.entity.SpoornBountyEntityRegistry;
 import org.spoorn.spoornbountymobs.util.SpoornBountyMobsUtil;
 
 @Mixin(PlayerEntity.class)
 public class PlayerEntityMixin {
+
+    private static final MutableText BROADCAST = new TranslatableText("sbm.broadcast.levelup").formatted(Formatting.WHITE);
 
     /**
      * For testing player data persistence.
@@ -36,10 +45,19 @@ public class PlayerEntityMixin {
             playerDataComponent.incrementBountyKillCount(entityDataComponent.getSpoornBountyTier());
 
             // Update player's highest tier if increased
-            int highestTier = playerDataComponent.getHighestBountyHunterLevel();
-            int currTier = SpoornBountyMobsUtil.getBountyHunterLevel(player);
-            if (currTier > highestTier) {
-                playerDataComponent.setHighestBountyHunterLevel(currTier);
+            int highestLevel = playerDataComponent.getHighestBountyHunterLevel();
+            int currLevel = SpoornBountyMobsUtil.getBountyHunterLevel(player);
+            if (currLevel > highestLevel) {
+                playerDataComponent.setHighestBountyHunterLevel(currLevel);
+                try {
+                    if (ModConfig.get().broadcastMessageWhenBountyLevelUp) {
+                        MutableText playerpart = new LiteralText(player.getDisplayName().getString()).formatted(Formatting.DARK_AQUA);
+                        MutableText levelpart = new LiteralText(Integer.toString(currLevel)).formatted(Formatting.LIGHT_PURPLE);
+                        player.getServer().getPlayerManager().broadcast(playerpart.append(BROADCAST).append(levelpart), MessageType.CHAT, Util.NIL_UUID);
+                    }
+                } catch (Exception e) {
+                    System.err.println("Error broadcasting SpoornBountyMobs level up: " + e);
+                }
             }
 
             // Sync new player data to clients
