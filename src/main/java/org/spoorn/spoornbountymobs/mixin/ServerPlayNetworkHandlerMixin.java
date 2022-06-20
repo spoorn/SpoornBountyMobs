4b2f9e36da07832d1196d49ab06f08e40464d975
spoorn <spookump@gmail.com>
2022-06-19 19:33:39 -0700

    Update compatibility with JamiesWhiteShirt/reach-entity-attributes.
    Fixes https://github.com/spoorn/SpoornBountyMobs/issues/10

--- src/main/java/org/spoorn/spoornbountymobs/mixin/ServerPlayNetworkHandlerMixin.java
+++ src/main/java/org/spoorn/spoornbountymobs/mixin/ServerPlayNetworkHandlerMixin.java
@@ -1,11 +1,9 @@
 package org.spoorn.spoornbountymobs.mixin;
 
 import net.minecraft.entity.Entity;
-import net.minecraft.network.packet.c2s.play.PlayerInteractEntityC2SPacket;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.world.ServerWorld;
-import org.objectweb.asm.Opcodes;
+import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,23 +11,30 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spoorn.spoornbountymobs.entity.component.EntityDataComponent;
 import org.spoorn.spoornbountymobs.util.SpoornBountyMobsUtil;
 
-@Mixin(ServerPlayNetworkHandler.class)
+@Mixin(value = ServerPlayNetworkHandler.class)
 public class ServerPlayNetworkHandlerMixin {
 
     @Shadow public ServerPlayerEntity player;
-
-    @Redirect(method = "onPlayerInteractEntity", at = @At(value = "FIELD", opcode = Opcodes.GETSTATIC, target = "Lnet/minecraft/server/network/ServerPlayNetworkHandler;MAX_BREAK_SQUARED_DISTANCE:D"))
-    private double scaleMaxDistanceForInteractingWithEntity(PlayerInteractEntityC2SPacket packet) {
-        ServerWorld serverWorld = this.player.getWorld();
-        final Entity entity = packet.getEntity(serverWorld);
-        if (SpoornBountyMobsUtil.entityIsHostileAndHasBounty(entity)) {
-            EntityDataComponent entityDataComponent = SpoornBountyMobsUtil.getSpoornEntityDataComponent(entity);
+    
+    /**
+     * This redirects the squaredDistanceTo() and divides by squared scale, instead of modifying the right-hand side
+     * of the expression `...squaredDistanceTo(Entity) < MAX_SQUARED_DISTANCE` to be compatible with mods that modify 
+     * that constant such as JamiesWhiteShirt/reach-entity-attributes:
+     * https://github.com/JamiesWhiteShirt/reach-entity-attributes/blob/1.18.2/src/main/java/com/jamieswhiteshirt/reachentityattributes/mixin/ServerPlayNetworkHandlerMixin.java#L22-L27
+     *
+     * Various mods such as Bewitchment make use of that library.
+     */
+    @Redirect(method = "onPlayerInteractEntity", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;squaredDistanceTo(Lnet/minecraft/util/math/Vec3d;)D"))
+    private double scaleMaxDistanceForInteractingWithEntity(Entity instance, Vec3d vector) {
+        double original = instance.squaredDistanceTo(vector);
+        if (SpoornBountyMobsUtil.entityIsHostileAndHasBounty(instance)) {
+            EntityDataComponent entityDataComponent = SpoornBountyMobsUtil.getSpoornEntityDataComponent(instance);
             float scale = entityDataComponent.getSpoornBountyTier().getMobSizeScale();
             if (scale > 1) {
-                return ServerPlayNetworkHandler.MAX_BREAK_SQUARED_DISTANCE * scale * scale;
+                return original / scale / scale;
             }
         }
         
-        return ServerPlayNetworkHandler.MAX_BREAK_SQUARED_DISTANCE;
+        return original;
     }
 }
