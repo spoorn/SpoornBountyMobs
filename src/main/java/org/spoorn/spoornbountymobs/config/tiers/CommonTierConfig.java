434ca2c37faa5aac6295a307a6eedcd2db85ce8a
spoorn <spookump@gmail.com>
2022-08-02 22:40:36 -0700

    Add default preconfigured drops for all tiers
    NOTE: IF YOU ALREADY HAD THIS MOD INTALLED PREVIOUSLY, YOU'LL NEED TO
    DELETE THE CONFIG FILE AT config/spoornbountymobs.json5 AND RESTART YOUR
    GAME TO GENERATE A NEW CONFIG FILE AND PICK UP THE NEW PRECONFIGURED DROPS

--- src/main/java/org/spoorn/spoornbountymobs/config/tiers/CommonTierConfig.java
+++ src/main/java/org/spoorn/spoornbountymobs/config/tiers/CommonTierConfig.java
@@ -2,6 +2,7 @@ package org.spoorn.spoornbountymobs.config.tiers;
 
 import draylar.omegaconfig.api.Comment;
 import org.spoorn.spoornbountymobs.config.Drop;
+import org.spoorn.spoornbountymobs.config.WeightedItem;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -104,5 +105,17 @@ public class CommonTierConfig {
     // See https://github.com/shedaniel/cloth-config/issues/104.
     // Shouldn't happen now that I switched to OmegaConfig?
     @Comment(EXAMPLE_LOOT_JSON)
-    public List<Drop> drops = new ArrayList<>();
+    public List<Drop> drops = new ArrayList<>(List.of(
+            new Drop(".*", 0.5, 1, new ArrayList<>(List.of(
+                    new WeightedItem("minecraft:diamond", 1),
+                    new WeightedItem("minecraft:emerald", 1),
+                    new WeightedItem("minecraft:.*_(sword|axe|shovel|pickaxe|hoe|helmet|chestplate|leggings|boots)", 1),
+                    new WeightedItem("minecraft:.*ingot", 1)
+            ))),
+            new Drop(".*", 0.5, 2, new ArrayList<>(List.of(
+                    new WeightedItem("minecraft:.*golden_apple", 10),
+                    new WeightedItem("minecraft:golden_carrot", 10),
+                    new WeightedItem("minecraft:cake", 1)
+            )))
+    ));
 }
--- src/main/java/org/spoorn/spoornbountymobs/config/tiers/RareTierConfig.java
+++ src/main/java/org/spoorn/spoornbountymobs/config/tiers/RareTierConfig.java
@@ -2,6 +2,7 @@ package org.spoorn.spoornbountymobs.config.tiers;
 
 import draylar.omegaconfig.api.Comment;
 import org.spoorn.spoornbountymobs.config.Drop;
+import org.spoorn.spoornbountymobs.config.WeightedItem;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,5 +31,21 @@ public class RareTierConfig {
     public double milestoneDamageIncrease = 0.3;
 
     @Comment("Drops data.  See COMMON_TIER's config for example configuration schema and documentation.")
-    public List<Drop> drops = new ArrayList<>();
+    public List<Drop> drops = new ArrayList<>(List.of(
+            new Drop(".*", 1.0, 2, new ArrayList<>(List.of(
+                    new WeightedItem("minecraft:diamond", 1),
+                    new WeightedItem("minecraft:emerald", 1),
+                    new WeightedItem("minecraft:.*_(sword|axe|shovel|pickaxe|hoe|helmet|chestplate|leggings|boots)", 1),
+                    new WeightedItem("minecraft:.*ingot", 1)
+            ))),
+            new Drop(".*", 1.0, 3, new ArrayList<>(List.of(
+                    new WeightedItem("minecraft:.*golden_apple", 10),
+                    new WeightedItem("minecraft:golden_carrot", 10),
+                    new WeightedItem("minecraft:cake", 1)
+            ))),
+            new Drop(".*", 0.5, 2, new ArrayList<>(List.of(
+                    new WeightedItem("minecraft:ender_pearl", 1),
+                    new WeightedItem("minecraft:firework_rocket", 1)
+            )))
+    ));
 }
